29b02af20156b9b5c1980e58edbdbdb7e1c58ea6
Jeremy Hilgar <jeremyhilgar@live.com>
2026-01-06 08:10:07 -0800

    leap.nvim

--- init.lua
+++ init.lua
@@ -1,11 +1,37 @@
+-- "there are many like it, but this one is mine"
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+
 vim.pack.add{
-    { src = 'https://github.com/catppuccin/nvim' },
+    { src = 'https://github.com/folke/tokyonight.nvim' },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/ibhagwan/fzf-lua' },
     { src = 'https://github.com/akinsho/bufferline.nvim' },
+    { src = 'https://github.com/folke/drop.nvim' },
+    { src = 'https://codeberg.org/andyg/leap.nvim' },
+    { src = 'https://github.com/itchyny/lightline.vim' },
 }
 
-vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
+vim.keymap.set('n', 'H', '^')
+vim.keymap.set('n', 'L', '$')
+vim.keymap.set('n', '<C-j>', '<Esc>')
+vim.keymap.set('i', '<C-j>', '<Esc>')
+vim.keymap.set('v', '<C-j>', '<Esc>')
+vim.keymap.set('s', '<C-j>', '<Esc>')
+vim.keymap.set('x', '<C-j>', '<Esc>')
+vim.keymap.set('c', '<C-j>', '<Esc>')
+vim.keymap.set('o', '<C-j>', '<Esc>')
+vim.keymap.set('l', '<C-j>', '<Esc>')
+vim.keymap.set('t', '<C-j>', '<Esc>')
+vim.keymap.set('n', '<up>', '<nop>')
+vim.keymap.set('n', '<down>', '<nop>')
+vim.keymap.set('i', '<up>', '<nop>')
+vim.keymap.set('i', '<down>', '<nop>')
+vim.keymap.set('i', '<left>', '<nop>')
+vim.keymap.set('i', '<right>', '<nop>')
+vim.keymap.set('n', '<left>', ':bp<cr>')
+vim.keymap.set('n', '<right>', ':bn<cr>')
 
 vim.keymap.set(
 	'n',
@@ -13,6 +39,8 @@ vim.keymap.set(
 	function() require('fzf-lua').files() end,
 	 { desc = 'find files' }
  )
+
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
 
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -30,12 +58,11 @@ require("bufferline").setup{
       show_close_icon = false,
   }
 }
-
 vim.keymap.set('n', '<leader>bp', '<cmd>BufferLinePick<CR>', { desc = 'pick buffer' })
 vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'prev buffer' })
 vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<CR>', { desc = 'next buffer' })
 
-vim.cmd("colorscheme catppuccin-macchiato")
+vim.cmd("colorscheme tokyonight")
 
 vim.diagnostic.config{
     virtual_text = true,
@@ -51,6 +78,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, bufopts)
     end,
 })
+
 vim.lsp.enable('ruff')
 
 vim.lsp.config('ty', {
